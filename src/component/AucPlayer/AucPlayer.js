0d5fa37163a5e2d3e0e1c9b881fae045b48789de
rabiulislamrubel <robiul91912113@gmail.com>
2021-04-27 15:09:01 +0600

    same player double add alart feature add

--- src/component/AucPlayer/AucPlayer.js
+++ src/component/AucPlayer/AucPlayer.js
@@ -5,32 +5,41 @@ import SinglePlayer from '../SinglePlayerList/SinglePlayer';
 import './AucPlayer.css';
 
 const AucPlayer = () => {
-    const [playerData, setPlayerData] = useState([])
-    const [singlePlayerData, setSinglePlayer]=useState([]);
+  const [playerData, setPlayerData] = useState([]);
+  const [singlePlayerData, setSinglePlayer] = useState([]);
 
-    useEffect(()=>{
-      fetch('https://www.json-generator.com/api/json/get/cpyqBqPQfC?indent=2')
-      .then(res => res.json())
-      .then(data => setPlayerData(data))
-    },[])
+  useEffect(() => {
+    fetch('https://www.json-generator.com/api/json/get/cpyqBqPQfC?indent=2')
+      .then((res) => res.json())
+      .then((data) => setPlayerData(data));
+  }, []);
 
-    const playerSelectHandle = (event) => {
-        const newData = [...singlePlayerData, event];
-        setSinglePlayer(newData);
+  const playerSelectHandle = (event) => {
+    const items = singlePlayerData.indexOf(event);
+    if (items === -1) {
+      const newData = [...singlePlayerData, event];
+      setSinglePlayer(newData);
+    } else {
+      alert('Player is Added');
     }
+  };
 
-    return (
-        <div className='player'> 
-            <div className='auc-player'>
-                {
-                    playerData.map(single => <SinglePlayer playerDetail={single} key={single._id} handler ={playerSelectHandle}></SinglePlayer>)
-                }
-            </div>
-            <div className='select-player'>
-                <SelectedPlayer selectPlayer = {singlePlayerData}></SelectedPlayer>
-            </div>
-        </div>
-    )
+  return (
+    <div className='player'>
+      <div className='auc-player'>
+        {playerData.map((single) => (
+          <SinglePlayer
+            playerDetail={single}
+            key={single._id}
+            handler={playerSelectHandle}
+          ></SinglePlayer>
+        ))}
+      </div>
+      <div className='select-player'>
+        <SelectedPlayer selectPlayer={singlePlayerData}></SelectedPlayer>
+      </div>
+    </div>
+  );
 };
 
 export default AucPlayer;
--- src/component/SelectedPlayer/SelectedPlayer.js
+++ src/component/SelectedPlayer/SelectedPlayer.js
@@ -3,22 +3,26 @@ import SelectSingleDetails from '../SelectSingleDetails/SelectSingleDetails';
 import './SelectedPlayer.css';
 
 const SelectedPlayer = (props) => {
-    console.log(props.selectPlayer)
-    const totalPlayerCost = props.selectPlayer.reduce((total,value)=> total += value.salary , 0);
-    return (
-        <div >
-            <div className='selected-player'>
-                <h2>Player are Selected: {props.selectPlayer.length}</h2>
-                <h4>Selected Player Cost: ${totalPlayerCost}</h4>
-            </div>
-            <div className='singel-details'>
-                {
-                    props.selectPlayer.map(spd => <SelectSingleDetails details={spd}></SelectSingleDetails>)
-                }
-                
-            </div>
-        </div>
-    );
+  const totalPlayerCost = props.selectPlayer.reduce(
+    (total, value) => (total += value.salary),
+    0
+  );
+  return (
+    <div>
+      <div className='selected-player'>
+        <h2>Player are Selected: {props.selectPlayer.length}</h2>
+        <h4>Selected Player Cost: ${totalPlayerCost}</h4>
+      </div>
+      <div className='singel-details'>
+        {props.selectPlayer.map((spd) => (
+          <SelectSingleDetails
+            details={spd}
+            key={spd._id}
+          ></SelectSingleDetails>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SelectedPlayer;
